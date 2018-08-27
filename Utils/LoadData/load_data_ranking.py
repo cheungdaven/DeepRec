0ee5102f2c7ca26026bf59ff115653edffc1d9ef
Shuai Zhang <cheungdaven@gmail.com>
2018-08-27 14:17:55 +1000

    Update load_data_ranking.py

--- Utils/LoadData/load_data_ranking.py
+++ Utils/LoadData/load_data_ranking.py
@@ -104,3 +104,54 @@ def load_data_neg(path="B:/Datasets/MovieLens/ml-100k/u.data", header = ['user_i
     print("Load data finished. Number of users:", n_users, "Number of items:", n_items)
     return train_matrix.todok(), test_dict, n_users, n_items
 
+def load_data_separately(path_train=None, path_test=None, path_val=None, header=['user_id', 'item_id', 'rating'],
+                         sep=" ", n_users=0, n_items=0):
+    n_users = n_users 
+    n_items = n_items  
+    print("start")
+    train_matrix = None
+    if path_train is not None:
+        train_data = pd.read_csv(path_train, sep=sep, names=header, engine='python')
+        print("Load data finished. Number of users:", n_users, "Number of items:", n_items)
+
+        train_row = []
+        train_col = []
+        train_rating = []
+
+        for line in train_data.itertuples():
+            u = line[1] #  - 1
+            i = line[2] #  - 1
+            train_row.append(u)
+            train_col.append(i)
+            train_rating.append(1)
+
+        train_matrix = csr_matrix((train_rating, (train_row, train_col)), shape=(n_users, n_items))
+        
+    print("Load data finished. Number of users:", n_users, "Number of items:", n_items)
+    test_dict = None
+    if path_test is not None:
+        test_data = pd.read_csv(path_test, sep=sep, names=header, engine='python')
+        test_row = []
+        test_col = []
+        test_rating = []
+        for line in test_data.itertuples():
+            test_row.append(line[1])
+            i = line[2]  # - 1
+            test_col.append(i)
+            test_rating.append(1)
+
+        test_matrix = csr_matrix((test_rating, (test_row, test_col)), shape=(n_users, n_items))
+
+        test_dict = {}
+        for u in range(n_users):
+            test_dict[u] = test_matrix.getrow(u).nonzero()[1]
+    all_items = set(np.arange(n_items))
+    train_interaction_matrix = []
+    for u in range(n_users):
+        train_interaction_matrix.append(list(train_matrix.getrow(u).toarray()[0]))
+
+    if path_val is not None:
+        val_data = pd.read_csv(path_val, sep=sep, names=header, engine='python')
+
+    print("end")
+    return train_interaction_matrix, test_dict, n_users, n_items
