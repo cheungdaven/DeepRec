c1293629216950ee0f5d0cfb154f60d31ab90194
mercileesb <mercileesb@coupang.com>
2019-05-28 19:18:23 +0900

    Modified issues

--- test/testSeqRec.py
+++ test/testSeqRec.py
@@ -1,5 +1,7 @@
 import argparse
 import tensorflow as tf
+import sys
+import os
 
 from models.seq_rec.Caser import Caser
 from models.seq_rec.AttRec import AttRec
@@ -7,6 +9,7 @@ from models.seq_rec.PRME import PRME
 from utils.load_data.load_data_seq import DataSet
 from utils.load_data.load_data_ranking import *
 
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
 def parse_args():
     parser = argparse.ArgumentParser(description='DeepRec')
@@ -40,10 +43,10 @@ if __name__ == '__main__':
         # Model selection
 
         if args.model == "Caser":
-            train_data = DataSet(path="../Data/ml100k/temp/train.dat", sep="\t",
+            train_data = DataSet(path="../data/ml100k/temp/train.dat", sep="\t",
                                  header=['user', 'item', 'rating', 'time'],
                                  isTrain=True, seq_len=5, target_len=3, num_users=943, num_items=1682)
-            test_data = DataSet(path="../Data/ml100k/temp/test.dat", sep="\t",
+            test_data = DataSet(path="../data/ml100k/temp/test.dat", sep="\t",
                                 header=['user', 'item', 'rating', 'time'],
                                 user_map=train_data.user_map, item_map=train_data.item_map)
             # train_data = DataSet(path="../Data/ml100k/seq/train.dat",  isTrain=True)
@@ -52,14 +55,14 @@ if __name__ == '__main__':
             model.build_network(L = train_data.sequences.L, num_T=train_data.sequences.T)
             model.execute(train_data, test_data)
         if args.model == "PRME":
-            train_data = DataSet(path="../Data/ml100k/temp/train.dat", sep="\t",header=['user', 'item', 'rating', 'time'],isTrain=True, seq_len=1, target_len=1)
-            test_data = DataSet(path="../Data/ml100k/temp/test.dat", sep="\t", header=['user', 'item', 'rating', 'time'], user_map=train_data.user_map, item_map=train_data.item_map)
+            train_data = DataSet(path="../data/ml100k/temp/train.dat", sep="\t",header=['user', 'item', 'rating', 'time'],isTrain=True, seq_len=1, target_len=1)
+            test_data = DataSet(path="../data/ml100k/temp/test.dat", sep="\t", header=['user', 'item', 'rating', 'time'], user_map=train_data.user_map, item_map=train_data.item_map)
             model = PRME(sess, train_data.num_user,  train_data.num_item)
             model.build_network(L = train_data.sequences.L, num_T=train_data.sequences.T)
             model.execute(train_data, test_data)
         if args.model == "AttRec":
-            train_data = DataSet(path="../Data/ml100k/temp/train.dat", sep="\t",header=['user', 'item', 'rating', 'time'],isTrain=True, seq_len=5, target_len=3, num_users=943, num_items=1682)
-            test_data = DataSet(path="../Data/ml100k/temp/test.dat", sep="\t", header=['user', 'item', 'rating', 'time'], user_map=train_data.user_map, item_map=train_data.item_map)
+            train_data = DataSet(path="../data/ml100k/temp/train.dat", sep="\t",header=['user', 'item', 'rating', 'time'],isTrain=True, seq_len=5, target_len=3, num_users=943, num_items=1682)
+            test_data = DataSet(path="../data/ml100k/temp/test.dat", sep="\t", header=['user', 'item', 'rating', 'time'], user_map=train_data.user_map, item_map=train_data.item_map)
             model = AttRec(sess, train_data.num_user,  train_data.num_item)
             # print(train_data.user_map)
             # print(train_data.item_map)
