de81f795aa6e8b03304ee9b40fe80ba75976cb2b
buracagyang <15591875898@163.com>
2019-07-12 16:04:00 +0800

    fixed: Fixed code that does not conform to the Python PEP8.
    Fixed code that does not conform to the Python PEP8.All modifications have been tested in scripts `test_item_ranking.py`

--- models/item_ranking/cdae.py
+++ models/item_ranking/cdae.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 """Implementation of CDAE.
-Reference: Wu, Yao, et al. "Collaborative denoising auto-encoders for top-n recommender systems." Proceedings of the Ninth ACM International Conference on Web Search and Data Mining. ACM, 2016.
+Reference: Wu, Yao, et al. "Collaborative denoising auto-encoders for top-n recommender systems." Proceedings
+of the Ninth ACM International Conference on Web Search and Data Mining. ACM, 2016.
 """
 
 import tensorflow as tf
@@ -19,20 +20,35 @@ __email__ = "cheungdaven@gmail.com"
 __status__ = "Development"
 
 
-class CDAE():
+class CDAE(object):
     def __init__(self, sess, num_user, num_item, learning_rate=0.01, reg_rate=0.01, epoch=500, batch_size=100,
-                 verbose=False, T=1, display_step=1000):
-        self.learning_rate = learning_rate
-        self.epochs = epoch
-        self.batch_size = batch_size
-        self.reg_rate = reg_rate
+                 verbose=False, t=1, display_step=1000):
         self.sess = sess
         self.num_user = num_user
         self.num_item = num_item
+        self.learning_rate = learning_rate
+        self.reg_rate = reg_rate
+        self.epochs = epoch
+        self.batch_size = batch_size
         self.verbose = verbose
-        self.T = T
+        self.T = t
         self.display_step = display_step
-        print("CDAE.")
+
+        self.user_id = None
+        self.corrupted_rating_matrix = None
+        self.rating_matrix = None
+        self.corruption_level = None
+        self.layer_2 = None
+        self.loss = None
+        self.optimizer = None
+        self.train_data = None
+        self.neg_items = None
+        self.num_training = None
+        self.total_batch = None
+        self.test_data = None
+        self.test_users = None
+        self.reconstruction = None
+        print("You are running CDAE.")
 
     def build_network(self, hidden_neuron=500, corruption_level=0):
         self.corrupted_rating_matrix = tf.placeholder(dtype=tf.float32, shape=[None, self.num_item])
@@ -40,21 +56,21 @@ class CDAE():
         self.user_id = tf.placeholder(dtype=tf.int32, shape=[None])
         self.corruption_level = corruption_level
 
-        W = tf.Variable(tf.random_normal([self.num_item, hidden_neuron], stddev=0.01))
-        W_prime = tf.Variable(tf.random_normal([hidden_neuron, self.num_item], stddev=0.01))
-        V = tf.Variable(tf.random_normal([self.num_user, hidden_neuron], stddev=0.01))
+        _W = tf.Variable(tf.random_normal([self.num_item, hidden_neuron], stddev=0.01))
+        _W_prime = tf.Variable(tf.random_normal([hidden_neuron, self.num_item], stddev=0.01))
+        _V = tf.Variable(tf.random_normal([self.num_user, hidden_neuron], stddev=0.01))
 
         b = tf.Variable(tf.random_normal([hidden_neuron], stddev=0.01))
         b_prime = tf.Variable(tf.random_normal([self.num_item], stddev=0.01))
-        print(np.shape(tf.matmul(self.corrupted_rating_matrix, W)))
-        print(np.shape(tf.nn.embedding_lookup(V, self.user_id)))
-        layer_1 = tf.sigmoid(tf.matmul(self.corrupted_rating_matrix, W) + tf.nn.embedding_lookup(V, self.user_id) + b)
-        self.layer_2 = tf.sigmoid(tf.matmul(layer_1, W_prime) + b_prime)
+        print(np.shape(tf.matmul(self.corrupted_rating_matrix, _W)))
+        print(np.shape(tf.nn.embedding_lookup(_V, self.user_id)))
+        layer_1 = tf.sigmoid(tf.matmul(self.corrupted_rating_matrix, _W) + tf.nn.embedding_lookup(_V, self.user_id) + b)
+        self.layer_2 = tf.sigmoid(tf.matmul(layer_1, _W_prime) + b_prime)
 
         self.loss = - tf.reduce_sum(
-            self.rating_matrix * tf.log(self.layer_2) + (1 - self.rating_matrix) * tf.log(1 - self.layer_2)) \
-                    + self.reg_rate * (
-        tf.nn.l2_loss(W) + tf.nn.l2_loss(W_prime) + tf.nn.l2_loss(V) + tf.nn.l2_loss(b) + tf.nn.l2_loss(b_prime))
+            self.rating_matrix * tf.log(self.layer_2) + (1 - self.rating_matrix) * tf.log(1 - self.layer_2)) + \
+            self.reg_rate * (tf.nn.l2_loss(_W) + tf.nn.l2_loss(_W_prime) + tf.nn.l2_loss(_V) +
+                             tf.nn.l2_loss(b) + tf.nn.l2_loss(b_prime))
 
         self.optimizer = tf.train.AdamOptimizer(learning_rate=self.learning_rate).minimize(self.loss)
 
@@ -68,7 +84,6 @@ class CDAE():
         print("data preparation finished.")
 
     def train(self):
-
         idxs = np.random.permutation(self.num_training)  # shuffled ordering
 
         for i in range(self.total_batch):
@@ -101,8 +116,8 @@ class CDAE():
         self.sess.run(init)
         for epoch in range(self.epochs):
             self.train()
-            if (epoch) % self.T == 0:
-                print("Epoch: %04d; " % (epoch), end='')
+            if epoch % self.T == 0:
+                print("Epoch: %04d; " % epoch, end='')
                 self.test()
 
     def save(self, path):
@@ -112,7 +127,8 @@ class CDAE():
     def predict(self, user_id, item_id):
         return np.array(self.reconstruction[np.array(user_id), np.array(item_id)])
 
-    def _data_process(self, data):
+    @staticmethod
+    def _data_process(data):
         return np.asmatrix(data)
 
     def _get_neg_items(self, data):
@@ -123,27 +139,43 @@ class CDAE():
 
         return neg_items
 
-    def _get_corrupted_input(self, input, corruption_level):
-        return np.random.binomial(n=1, p=1 - corruption_level) * input
+    @staticmethod
+    def _get_corrupted_input(input_train_data, corruption_level):
+        return np.random.binomial(n=1, p=1 - corruption_level) * input_train_data
 
 
-class ICDAE():
-    '''
-    Based on CDAE and I-AutoRec, I designed the following item based CDAE, it seems to perform better than CDAE slightly.
-    '''
-
+class ICDAE(object):
+    """
+    Based on CDAE and I-AutoRec, I designed the following item based CDAE, it seems to perform better than CDAE
+    slightly.
+    """
     def __init__(self, sess, num_user, num_item, learning_rate=0.01, reg_rate=0.01, epoch=500, batch_size=300,
-                 verbose=False, T=2, display_step=1000):
-        self.learning_rate = learning_rate
-        self.epochs = epoch
-        self.batch_size = batch_size
-        self.reg_rate = reg_rate
+                 verbose=False, t=2, display_step=1000):
         self.sess = sess
         self.num_user = num_user
         self.num_item = num_item
+        self.learning_rate = learning_rate
+        self.reg_rate = reg_rate
+        self.epochs = epoch
+        self.batch_size = batch_size
         self.verbose = verbose
-        self.T = T
+        self.T = t
         self.display_step = display_step
+
+        self.corrupted_interact_matrix = None
+        self.interact_matrix = None
+        self.item_id = None
+        self.corruption_level = None
+        self.layer_2 = None
+        self.loss = None
+        self.optimizer = None
+        self.train_data = None
+        self.neg_items = None
+        self.num_training = None
+        self.total_batch = None
+        self.test_data = None
+        self.test_users = None
+        self.reconstruction = None
         print("Item based CDAE.")
 
     def build_network(self, hidden_neuron=500, corruption_level=0):
@@ -152,21 +184,21 @@ class ICDAE():
         self.item_id = tf.placeholder(dtype=tf.int32, shape=[None])
         self.corruption_level = corruption_level
 
-        W = tf.Variable(tf.random_normal([self.num_user, hidden_neuron], stddev=0.01))
-        W_prime = tf.Variable(tf.random_normal([hidden_neuron, self.num_user], stddev=0.01))
-        V = tf.Variable(tf.random_normal([self.num_item, hidden_neuron], stddev=0.01))
+        _W = tf.Variable(tf.random_normal([self.num_user, hidden_neuron], stddev=0.01))
+        _W_prime = tf.Variable(tf.random_normal([hidden_neuron, self.num_user], stddev=0.01))
+        _V = tf.Variable(tf.random_normal([self.num_item, hidden_neuron], stddev=0.01))
 
         b = tf.Variable(tf.random_normal([hidden_neuron], stddev=0.01))
         b_prime = tf.Variable(tf.random_normal([self.num_user], stddev=0.01))
-        # print(np.shape(tf.matmul(self.corrupted_interact_matrix, W)))
-        # print(np.shape( tf.nn.embedding_lookup(V, self.item_id)))
-        layer_1 = tf.sigmoid(tf.matmul(self.corrupted_interact_matrix, W) + b)
-        self.layer_2 = tf.sigmoid(tf.matmul(layer_1, W_prime) + b_prime)
+        # print(np.shape(tf.matmul(self.corrupted_interact_matrix, _W)))
+        # print(np.shape( tf.nn.embedding_lookup(_V, self.item_id)))
+        layer_1 = tf.sigmoid(tf.matmul(self.corrupted_interact_matrix, _W) + b)
+        self.layer_2 = tf.sigmoid(tf.matmul(layer_1, _W_prime) + b_prime)
 
         self.loss = - tf.reduce_sum(
-            self.interact_matrix * tf.log(self.layer_2) + (1 - self.interact_matrix) * tf.log(1 - self.layer_2)) \
-                    + self.reg_rate * (
-        tf.nn.l2_loss(W) + tf.nn.l2_loss(W_prime) + tf.nn.l2_loss(b) + tf.nn.l2_loss(b_prime))
+            self.interact_matrix * tf.log(self.layer_2) + (1 - self.interact_matrix) * tf.log(1 - self.layer_2)) + \
+            self.reg_rate * (tf.nn.l2_loss(_W) + tf.nn.l2_loss(_W_prime) +
+                             tf.nn.l2_loss(b) + tf.nn.l2_loss(b_prime))
 
         self.optimizer = tf.train.AdamOptimizer(learning_rate=self.learning_rate).minimize(self.loss)
 
@@ -180,7 +212,6 @@ class ICDAE():
         print("data preparation finished.")
 
     def train(self):
-
         idxs = np.random.permutation(self.num_training)  # shuffled ordering
 
         for i in range(self.total_batch):
@@ -213,8 +244,8 @@ class ICDAE():
         self.sess.run(init)
         for epoch in range(self.epochs):
             self.train()
-            if (epoch) % self.T == 0:
-                print("Epoch: %04d; " % (epoch), end='')
+            if epoch % self.T == 0:
+                print("Epoch: %04d; " % epoch, end='')
                 self.test()
 
     def save(self, path):
@@ -224,7 +255,8 @@ class ICDAE():
     def predict(self, user_id, item_id):
         return np.array(self.reconstruction[np.array(user_id), np.array(item_id)])
 
-    def _data_process(self, data):
+    @staticmethod
+    def _data_process(data):
         return np.asmatrix(data)
 
     def _get_neg_items(self, data):
@@ -235,5 +267,6 @@ class ICDAE():
 
         return neg_items
 
-    def _get_corrupted_input(self, input, corruption_level):
-        return np.random.binomial(n=1, p=1 - corruption_level) * input
+    @staticmethod
+    def _get_corrupted_input(input_train_data, corruption_level):
+        return np.random.binomial(n=1, p=1 - corruption_level) * input_train_data
